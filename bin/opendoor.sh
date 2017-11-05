50e40224329ca61cc36dbc9101ebe3d9882ddfac
Gavilan Steinman <gavilan.steinman@gmail.com>
2017-11-05 03:16:53 +0000

    smile

--- bin/opendoor.sh
+++ bin/opendoor.sh
@@ -4,5 +4,5 @@ echo "out" > /sys/class/gpio/gpio26/direction
 echo "6" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio6/direction
 
-echo "1" > /sys/class/gpio/gpio26/value
-echo "1" > /sys/class/gpio/gpio6/value
+echo "0" > /sys/class/gpio/gpio26/value
+echo "0" > /sys/class/gpio/gpio6/value
